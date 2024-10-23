9cce70fda2edd30d430a642e46de90cd48be93d2
nkun0003 <nkun0003@algonquinlive.com>
2024-10-23 00:31:36 -0400

    Initial Commit

---
+++ MAD9137-SwiftRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MAD9137-SwiftRecipe
+//
+//  Created by Rocien Nkunga on 23/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MAD9137-SwiftRecipe/MAD9137_SwiftRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD9137_SwiftRecipeApp.swift
+//  MAD9137-SwiftRecipe
+//
+//  Created by Rocien Nkunga on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MAD9137_SwiftRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
